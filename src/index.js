2e1c68291acea691bc3454cf5c2838ea3401eda1
Shako Yevhen <schako15101992@gmail.com>
2022-09-09 20:16:07 +0300

    add user.json

--- src/index.js
+++ src/index.js
@@ -1,10 +1,43 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+
+
+// {/* < div class="profile" >
+//   <div class="description">
+//     <img
+//       src="{user.avatar}"
+//       alt="User avatar"
+//       class="avatar"
+//     />
+//     <p class="name">{user.username}</p>
+//     <p class="tag">{user.tag}</p>
+//     <p class="location">{user.location}</p>
+//   </div>
+
+//   <ul class="stats">
+//     <li>
+//       <span class="label">Followers</span>
+//       <span class="quantity">{user.stats.followers}</span>
+//     </li>
+//     <li>
+//       <span class="label">Views</span>
+//       <span class="quantity">{user.stats.views}</span>
+//     </li>
+//     <li>
+//       <span class="label">Likes</span>
+//       <span class="quantity">{user.stats.likes}</span>
+//     </li>
+//   </ul>
+// </div >
+//  */}
