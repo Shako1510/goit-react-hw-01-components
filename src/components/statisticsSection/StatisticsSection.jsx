9b3525373c7136784216fb2222f9cad3eddb8339
Shako Yevhen <schako15101992@gmail.com>
2022-11-03 01:55:38 +0200

    Підключив рандомний бекграунд в завд2
    Підключив інлайн

--- src/components/statisticsSection/StatisticsSection.jsx
+++ src/components/statisticsSection/StatisticsSection.jsx
@@ -3,6 +3,11 @@ import css from './StatisticsSection.module.css'
 
 
 export default function StatisticsSection({ title, stats }) {
+    const getRandomColor = () =>
+        `#${Math.floor(Math.random() * 16777215)
+            .toString(16)
+            .padEnd(6, '0')}`;
+
 
     return (
         < section className={css.sections}>
@@ -10,13 +15,13 @@ export default function StatisticsSection({ title, stats }) {
             {title && <h2 className={css.title}>{title}</h2>}
             <ul className={css.list}>
                 {stats.map(({ id, label, percentage }) => (
-                    <li className={css.item} key={id} >
+                    <li className={css.item} style={{ backgroundColor: getRandomColor() }} key={id} >
                         <span className="label">{label}</span>
-                        <span className={css.percentage}>{percentage}%</span>
+                        <span className={css.percentage} >{percentage}%</span>
                     </li>))},
             </ul>
 
-        </ section>
+        </ section >
     );
 };
 
@@ -30,6 +35,7 @@ StatisticsSection.propTypes = {
         }),
     ),
 };
+
 
 // const GetRandomColor = function () {
 //     return '#' + (Math.random() * 0xffffff << 0).toString(16);
